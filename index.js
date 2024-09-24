c6fe12c71a34294d7483beb1ea17b78f40027f11
bazanty <bazantyloffan@gmail.com>
2024-09-25 00:35:32 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun (activity ='roller-skate') { 
+return `This Saturday, I want to ${activity}!`
+} console.log (saturdayFun())
+
+function mondayWork (activity ="work from home." ) {
+    return "This Monday , I will ${activity}"
+}console.log (mondayWork())
+
+
+function mondayWork (activity ='go to the office') {
+    return `This Monday, I will ${activity}.`;
+}console.log (mondayWork())
+
+function wrapAdjective(skip = "*") {
+    return function(adjective = "special") {
+        return `You are ${skip}${adjective}${skip}!`;
+    };
+}
